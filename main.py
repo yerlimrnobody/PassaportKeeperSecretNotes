cf5e98f93b83ffc068f4f8f248cadaf6909c5791
yerlimrnobody <yerlimrnobody@gmail.com>
2024-06-13 16:56:11 +0300

    Encrypt and Decrypt Functions Added

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ import tkinter
 
 try:
     from PIL import Image, ImageTk
+    from cryptography.fernet import Fernet
+    import base64
+    import hashlib
+
 except:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "pillow"])
-
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "cryptography"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "base64"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "hashlib"])
 
 
 
@@ -60,18 +66,51 @@ label_widget_4.pack()
 entry_widget_2 = tkinter.Entry()
 entry_widget_2.pack()
 
+def generate_fernet_key(password):
+
+    hashed_password = hashlib.sha256(password.encode()).digest()
+    fernet_key = base64.urlsafe_b64encode(hashed_password)
+    return fernet_key
+
+def encrypt_string():
+    your_key = entry_widget_2.get()
+    try:
+        fernet_key = generate_fernet_key(your_key)
+        fernet_crypt = Fernet(fernet_key)
+        get_text_to_crypt = label_widget_text.get("1.0", "end-1c")
+        encrypted_text = fernet_crypt.encrypt(get_text_to_crypt.encode())
+        encrypted_out_of_function = encrypted_text.decode()
+        print(f"Şifrelenmiş Metin: {encrypted_text.decode()}")
+        return encrypted_out_of_function
+    except Exception as e:
+        print(f"Geçersiz anahtar: {e}")
+
+def decrypt_string():
+    your_key = entry_widget_2.get()
+    try:
+        fernet_key = generate_fernet_key(your_key)
+        fernet_crypt = Fernet(fernet_key)
+        get_text_to_decrypt = label_widget_text.get("1.0", "end-1c")
+        decrypted_text = fernet_crypt.decrypt(get_text_to_decrypt.encode()).decode()
+        print(f"Çözülmüş Metin: {decrypted_text}")
+    except Exception as e:
+        print(f"Geçersiz anahtar veya şifrelenmiş metin: {e}")
+
+
 def title_save():
     file_name = "data_file"
     try:
         with open(file_name, "r"):
             with open(file_name, "a") as stored_file:
                 stored_file.write(entry_widget_1.get()+"\n")
-                stored_file.write(label_widget_text.get("1.0","end-1c")+"\n")
+                encrypted_string = encrypt_string()
+                stored_file.write(encrypted_string + "\n")
 
     except FileNotFoundError:
         with open(file_name, "w") as stored_file:
             stored_file.write(entry_widget_1.get()+"\n")
-            stored_file.write(label_widget_text.get("1.0","end-1c" )+"\n")
+            encrypted_string = encrypt_string()
+            stored_file.write(encrypted_string + "\n")
 
 
 encrypt_button = tkinter.Button()
@@ -80,7 +119,7 @@ encrypt_button.pack()
 
 
 decrypt_button = tkinter.Button()
-decrypt_button.config(text="Decrypt")
+decrypt_button.config(text="Decrypt", command=decrypt_string)
 decrypt_button.pack()
 
 
